4302fae47be0f007d12b09b63738e5d0ad110918
Toby16 <bolajibaasit14@gmail.com>
2023-04-21 16:24:12 -0400

    Install a package | Report: added alternative in comments

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -4,3 +4,8 @@ exec {'install_flask':
   command => '/usr/bin/pip install flask==2.1.0',
   creates => '/usr/local/lib/python3.6/dist-packages/flask'
 }
+
+# package { 'puppet-lint':
+#   ensure   => '2.1.1',
+#   provider => 'gem'
+# }
