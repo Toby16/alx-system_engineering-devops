462f61d4560c82c7684fdd92d39a2c3e260f6088
Toby16 <bolajibaasit14@gmail.com>
2023-06-06 17:25:25 -0400

    Count it! | Report: Not done yet

---
+++ 0x16-api_advanced/100-count.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""
+a recursive function that queries the Reddit API,
+parses the title of all hot articles,
+and prints a sorted count of given keywords
+(case-insensitive, delimited by spaces.
+"""
